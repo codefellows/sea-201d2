1f27bfd75c22fc84aeb4f86101556515bc42c348
Scott Schmidt <sjschmidt44@gmail.com>
2015-12-15 07:57:55 -0800

    Update repo with pseudocode outline from Monday lecture.

---
+++ week-3/dec-14/bus_mall_demo/app.js
@@ -0,0 +1,49 @@
+// Create one array to hold ALL product objects
+var allProducts = [];
+// Create one array to hold the string name values of each image
+var productNames = ['boots', 'chair', 'scissors', 'water_can', 'wine_glass'];
+​
+// Object Constructor Notation (Many instances of an Object)
+function Product(name, path) {
+  // Constructor will define any paramaterized properties and any fixed value properties
+  // Will also push each object to the allProducts array when created
+}
+​
+(function buildAlbum() {
+  // For each element of the productNames array:
+    // Create a new instance of an object by concatenating strings and using the productNames values
+})();
+​
+//Object Literal Notation (Singular instance of an Object)
+var productRank = {
+  // Set the properties and values for the data you want to track; total clicks, product objects, and html elements
+​
+  getRandomIndex: function() {
+    // Get a random number between 0 and the length of your productNames array
+  },
+​
+  displayImages: function() {
+    // Get three random objects and assign them to the object properties for each of the three you've already defined
+​
+    if ('There are any duplicate image objects assigned') {
+      // Reroll the displayImages method
+    }
+​
+    // Assign and src and id to each element using the path and name of each related object
+  },
+​
+  showResults: function() {
+    if ('There is no remainder when dividing the total clicks by 15') {
+      // Use the hidden attribute of the button to show / hide
+    }
+  }
+};
+​
+// This is one of the three event listeners you will need to write, which listens for clicks on each element
+productRank.leftEl.addEventListener('click', function() {
+  // Increment the object's tally property and productRank's total clicks by 1
+  // Call the showResults method to check whether there have been 15 clicks
+  // Call the displayImages method to reroll three new images
+});
+​
+// Finally, call your displayImages method once on page load.
