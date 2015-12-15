d636d799317c95c1a92f2192b490043d81f52d2d
samhamm <samhamm@gmail.com>
2015-12-15 15:15:34 -0800

    more in-class edits to chart demo

--- week-3/dec-15/chart-demo/app.js
+++ week-3/dec-15/chart-demo/app.js
@@ -1,4 +1,4 @@
-var taValues = [34 ,67, 44, 93];
+var taValues = [34 ,67, 94, 93];
 
 var kevin = [
   {
@@ -24,18 +24,36 @@ var kevin = [
     label: 'Sam',
     color: '#6AE128',
     highlight: '#6AE199'
+  },
+  {
+    value : 33,
+    label: 'Kevin',
+    color: '#4AE128',
+    highlight: '#4AE199'
+  },
+  {
+    value : 114,
+    label: 'Nadia',
+    color: '#6AE128',
+    highlight: '#6AE199'
+  },
+  {
+    value : taValues[3] + taValues[1],
+    label: 'Trish',
+    color: '#6AE128',
+    highlight: '#6AE199'
   }
 ];
 
 var context = document.getElementById('popularity').getContext('2d');
 
-var popularityChart = new Chart(context).Doughnut(kevin, {
+var popularityChart = new Chart(context).PolarArea(kevin, {
     //Number - Amount of animation steps
-    animationSteps : 100,
+    animationSteps : 250,
     //String - Animation easing effect
     animationEasing : "easeOutBounce",
     //Boolean - Whether we animate the rotation of the Doughnut
-    animateRotate : false,
+    animateRotate : true,
     //Boolean - Whether we animate scaling the Doughnut from the centre
     animateScale : true,
     scaleShowLabelBackdrop : true
